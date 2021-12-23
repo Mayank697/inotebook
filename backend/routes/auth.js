f9b25a4737711f68cc5bca7945b86d5480a6090d
Mayank697 <smayank2000@gmail.com>
2021-12-23 15:02:55 +0530

    user-creation-validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 // Creating User: POST "/api/auth"
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    res.send("User Created Successfully");
+router.post('/', [
+    body('name').isLength({ min: 5 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(user => res.json(user))
+    .catch(err => {
+        console.log(err)
+        res.json({error: "Please enter a valid email"})
+    })
 });
 
 module.exports = router;
