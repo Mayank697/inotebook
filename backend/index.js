adf470fa5d143c7505e7a63f8402cefb932b0181
Mayank697 <smayank2000@gmail.com>
2021-12-23 12:02:57 +0530

    backend-setup-done

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const connectToMongo = require('./db');
+connectToMongo();
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
